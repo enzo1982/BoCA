bcbd19862d739b673b01d33b1e89cdbc0d238278
Robert Kausch <robert.kausch@freac.org>
2013-04-29 16:11:29 +0000

    Build system fixes.

--- cdk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set BUILD_UNIX=True
-
-echo "fre:ac Component Development Kit v1.1 Beta 1"
-echo "Copyright (C) 2001-2013 Robert Kausch"
-
-bash
--- cdk64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set BUILD_UNIX=True
-set BUILD_X64=True
-
-echo "fre:ac Component Development Kit v1.1 Beta 1 (x64)"
-echo "Copyright (C) 2001-2013 Robert Kausch"
-
-bash
