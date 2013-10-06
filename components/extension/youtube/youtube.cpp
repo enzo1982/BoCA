1448aff42918a657c6c964d5da64c9324f97fcd2
Robert Kausch <robert.kausch@freac.org>
2013-10-06 12:35:12 +0000

    Disable video downloader on OS X 10.5 and earlier.

--- components/extension/youtube/youtube.cpp
+++ components/extension/youtube/youtube.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2011 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2007-2013 Robert Kausch <robert.kausch@bonkenc.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -15,17 +15,38 @@
 
 const String &BoCA::YouTube::GetComponentSpecs()
 {
-	static String	 componentSpecs = "		\
-							\
-	  <?xml version=\"1.0\" encoding=\"UTF-8\"?>	\
-	  <component>					\
-	    <name>YouTube Video Downloader</name>	\
-	    <version>1.0</version>			\
-	    <id>youtube-ext</id>			\
-	    <type>extension</type>			\
-	  </component>					\
-							\
-	";
+	static String	 componentSpecs;
+
+#ifdef __APPLE__
+	/* Check if OS X version is at least 10.6 (Darwin 10.0) as
+	 * V8 does not work on earlier releases of OS X yet.
+	 */
+	Buffer<char>	 buffer(32);
+	FILE		*pstdin = popen("sysctl kern.osrelease", "r");
+
+	fscanf(pstdin, String("%[^\n]").Append(String::FromInt(buffer.Size() - 1)), (char *) buffer);
+
+	pclose(pstdin);
+
+	String		 osrelease = (char *) buffer;
+
+	if (!osrelease.StartsWith("kern.osrelease: ") || osrelease.Tail(osrelease.Length() - 16).ToInt() >= 10)
+	{
+#endif
+		componentSpecs = "				\
+								\
+		  <?xml version=\"1.0\" encoding=\"UTF-8\"?>	\
+		  <component>					\
+		    <name>YouTube Video Downloader</name>	\
+		    <version>1.0</version>			\
+		    <id>youtube-ext</id>			\
+		    <type>extension</type>			\
+		  </component>					\
+								\
+		";
+#ifdef __APPLE__
+	}
+#endif
 
 	return componentSpecs;
 }
