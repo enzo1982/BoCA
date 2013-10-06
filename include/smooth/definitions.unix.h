3ee5f87494f023e78b47483f9481b214fbb58c7a
Robert Kausch <robert.kausch@freac.org>
2013-10-06 12:45:30 +0000

    Updatedd smooth header files.

--- include/smooth/definitions.unix.h
+++ include/smooth/definitions.unix.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2011 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -14,11 +14,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-#define SMOOTHAPI
-#define SMOOTHVAR extern
+#define SMOOTHAPI __attribute__ ((visibility ("default")))
+#define SMOOTHVAR extern __attribute__ ((visibility ("default")))
 
-#define SMOOTH_PLUGIN_API
-#define SMOOTH_PLUGIN_VAR extern
+#define SMOOTH_PLUGIN_API __attribute__ ((visibility ("default")))
+#define SMOOTH_PLUGIN_VAR extern __attribute__ ((visibility ("default")))
 
 #ifndef __declspec
 #	define __declspec(x)
--- include/smooth/threads/rwlock.h
+++ include/smooth/threads/rwlock.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2012 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -30,7 +30,7 @@ namespace smooth
 		class SMOOTHAPI RWLock
 		{
 			private:
-				static const Short	 MAX_READ_LOCKS;
+				static const Short	 maxReadLocks;
 
 				Bool			 writeLocked;
 
