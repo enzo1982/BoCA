fa3775481330013bfc7af031dac8e70e8e5cdbfd
Robert Kausch <robert.kausch@freac.org>
2013-10-26 11:55:17 +0000

    Updated smooth header files.

--- include/smooth/gui/widgets/multi/menu/popupmenuentry.h
+++ include/smooth/gui/widgets/multi/menu/popupmenuentry.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2010 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -35,6 +35,8 @@ namespace smooth
 	{
 		class SMOOTHAPI PopupMenuEntry : public MenuEntry
 		{
+			private:
+				UnsignedInt64		 popupMenuClosed;
 			protected:
 				Hotspot			*hotspot;
 				PopupMenu		*owner;
