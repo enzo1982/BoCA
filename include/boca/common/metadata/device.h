1541e88fd109439ed5866a070c2bb2de63c4de66
Robert Kausch <robert.kausch@freac.org>
2014-02-14 18:47:55 +0000

    Added device path attribute to device info class.

--- include/boca/common/metadata/device.h
+++ include/boca/common/metadata/device.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2010 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2007-2014 Robert Kausch <robert.kausch@bonkenc.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -33,6 +33,7 @@ namespace BoCA
 			 */
 			DeviceType	 type;
 			String		 name;
+			String		 path;
 
 			Bool		 canOpenTray;
 
