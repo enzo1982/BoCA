87c76b4451307784b67fa0034511d28ad2c47c92
Robert Kausch <robert.kausch@freac.org>
2014-02-23 21:53:30 +0000

    Updated smooth header files.

--- include/smooth/gui/widgets/special/droparea.h
+++ include/smooth/gui/widgets/special/droparea.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2010 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -27,8 +27,6 @@ namespace smooth
 	{
 		class SMOOTHAPI DropArea : public Widget
 		{
-			private:
-				Bool					 initialized;
 			public:
 				static const Short			 classID;
 
