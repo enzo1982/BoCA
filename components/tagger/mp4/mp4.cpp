29a8f05aa6d2aad46803ce0d569b6c481fc91ba1
Robert Kausch <robert.kausch@freac.org>
2014-03-15 20:39:19 +0000

    Import cover art into chapters.

--- components/tagger/mp4/mp4.cpp
+++ components/tagger/mp4/mp4.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2013 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2007-2014 Robert Kausch <robert.kausch@bonkenc.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -319,6 +319,7 @@ Error BoCA::TaggerMP4::ParseStreamInfo(const String &fileName, Track &track)
 			Track	 rTrack;
 
 			rTrack.origFilename = track.origFilename;
+			rTrack.pictures	    = track.pictures;
 
 			rTrack.sampleOffset = Math::Round(Float(offset)			 / MP4_MSECS_TIME_SCALE * format.rate);
 			rTrack.length	    = Math::Round(Float(chapterList[i].duration) / MP4_MSECS_TIME_SCALE * format.rate);
