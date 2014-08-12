19415ce73b22341cad6cf6f57d9e6dcd9c31648f
Robert Kausch <robert.kausch@freac.org>
2014-08-12 22:23:32 +0000

    Updated smooth header files.

--- include/smooth/gui/widgets/multi/list/listbox.h
+++ include/smooth/gui/widgets/multi/list/listbox.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2010 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -67,6 +67,8 @@ namespace smooth
 				Int			 GetNthTabOffset(Int n)	const							{ return header->GetNthTabOffset(n); }
 				Int			 GetNthTabWidth(Int n) const							{ return header->GetNthTabWidth(n); }
 				Int			 GetNthTabOrientation(Int n) const						{ return header->GetNthTabOrientation(n); }
+			signals:
+				Signal1<Void, Int>	 onClickTab;
 			slots:
 				Void			 OnScrollbarValueChange();
 				Void			 OnChangeSize(const Size &);
--- include/smooth/gui/widgets/multi/list/listboxheader.h
+++ include/smooth/gui/widgets/multi/list/listboxheader.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2011 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -64,7 +64,11 @@ namespace smooth
 				Int			 GetNthTabOffset(Int) const;
 				Int			 GetNthTabWidth(Int) const;
 				Int			 GetNthTabOrientation(Int) const;
+			signals:
+				Signal1<Void, Int>	 onClickTab;
 			slots:
+				Void			 OnLeftButtonClick(const Point &);
+
 				Void			 OnMouseDragStart(const Point &);
 				Void			 OnMouseDrag(const Point &);
 				Void			 OnMouseDragEnd(const Point &);
