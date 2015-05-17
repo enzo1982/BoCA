1a768945d407f9d159b998517ce19bd060205b0c
Robert Kausch <robert.kausch@freac.org>
2015-05-17 11:15:08 +0000

    Adapted to smooth changes.

--- components/extension/tagedit/editor/editor_basic_cover.cpp
+++ components/extension/tagedit/editor/editor_basic_cover.cpp
@@ -1,5 +1,5 @@
  /* BonkEnc Audio Encoder
-  * Copyright (C) 2001-2013 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2001-2015 Robert Kausch <robert.kausch@bonkenc.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -44,7 +44,7 @@ BoCA::CoverDisplay::~CoverDisplay()
 
 const Error &BoCA::CoverDisplay::ShowDialog()
 {
-	window->Stay();
+	window->WaitUntilClosed();
 
 	return error;
 }
