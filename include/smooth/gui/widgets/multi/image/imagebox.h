50b473bce0d243cd1c9cdc84971889e8cc3349c6
Robert Kausch <robert.kausch@freac.org>
2015-06-13 11:49:53 +0000

    Updated smooth header files.

--- include/smooth/gui/widgets/multi/image/imagebox.h
+++ include/smooth/gui/widgets/multi/image/imagebox.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2010 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2015 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -32,7 +32,7 @@ namespace smooth
 				Scrollbar		*scrollbar;
 				Int			 scrollbarPos;
 
-				Int			 visibleEntriesChecksum;
+				String			 visibleEntriesString;
 
 				Int			 GetEntriesWidth() const;
 			public:
