aef2c40423ac394bec774e46b7168584caaccd5a
Robert Kausch <robert.kausch@freac.org>
2015-06-14 16:37:56 +0000

    Updated smooth header files.

--- include/smooth/gui/widgets/multi/list/list.h
+++ include/smooth/gui/widgets/multi/list/list.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2015 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -39,7 +39,7 @@ namespace smooth
 
 				virtual Int			 EnableLocking(Bool = True);
 
-				ListEntry			*AddEntry(const String &);
+				ListEntry			*AddEntry(const String &, Bool = False);
 				ListEntrySeparator		*AddSeparator();
 
 				Int				 Add(Widget *);
