8a1f440f6c564bbcacba8cb8f0e0ae1a76a14b1c
Robert Kausch <robert.kausch@freac.org>
2015-08-01 16:05:13 +0000

    Added channel layout definitions.

--- include/boca/common/metadata/format.h
+++ include/boca/common/metadata/format.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2014 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@bonkenc.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -25,6 +25,51 @@ namespace BoCA
 		BYTE_INTEL,
 
 		NUM_BYTE_ORDERS
+	};
+
+	namespace Channel
+	{
+		typedef UnsignedByte	 Layout[];
+
+		const UnsignedByte	 FrontLeft     =  0;
+		const UnsignedByte	 FrontRight    =  1;
+		const UnsignedByte	 FrontCenter   =  2;
+		const UnsignedByte	 LFE	       =  3;
+		const UnsignedByte	 RearLeft      =  4;
+		const UnsignedByte	 RearRight     =  5;
+		const UnsignedByte	 RearCenter    =  6;
+		const UnsignedByte	 SideLeft      =  7;
+		const UnsignedByte	 SideRight     =  8;
+		const UnsignedByte	 TopFrontLeft  =  9;
+		const UnsignedByte	 TopFrontRight = 10;
+
+		const Layout		 Default_2_0 = { FrontLeft, FrontRight };
+		const Layout		 Default_2_1 = { FrontLeft, FrontRight, LFE };
+		const Layout		 Default_3_0 = { FrontLeft, FrontRight, FrontCenter };
+		const Layout		 Default_3_1 = { FrontLeft, FrontRight, FrontCenter, LFE };
+		const Layout		 Default_4_0 = { FrontLeft, FrontRight, RearLeft, RearRight };
+		const Layout		 Default_4_1 = { FrontLeft, FrontRight, LFE, RearLeft, RearRight };
+		const Layout		 Default_5_0 = { FrontLeft, FrontRight, FrontCenter, RearLeft, RearRight };
+		const Layout		 Default_5_1 = { FrontLeft, FrontRight, FrontCenter, LFE, RearLeft, RearRight };
+		const Layout		 Default_6_1 = { FrontLeft, FrontRight, FrontCenter, LFE, RearCenter, SideLeft, SideRight };
+		const Layout		 Default_7_1 = { FrontLeft, FrontRight, FrontCenter, LFE, RearLeft, RearRight, SideLeft, SideRight };
+		const Layout		 Default_9_1 = { FrontLeft, FrontRight, FrontCenter, LFE, RearLeft, RearRight, SideLeft, SideRight, TopFrontLeft, TopFrontRight };
+
+		const Layout		 AAC_3_0     = { FrontCenter, FrontLeft, FrontRight };
+		const Layout		 AAC_5_0     = { FrontCenter, FrontLeft, FrontRight, RearLeft, RearRight };
+		const Layout		 AAC_5_1     = { FrontCenter, FrontLeft, FrontRight, RearLeft, RearRight, LFE };
+
+		const Layout		 AIFF_3_1    = { FrontLeft, FrontCenter, FrontRight, LFE };
+		const Layout		 AIFF_5_1    = { FrontLeft, RearLeft, FrontCenter, FrontRight, RearRight, LFE };
+
+		const Layout		 Vorbis_3_0  = { FrontLeft, FrontCenter, FrontRight };
+		const Layout		 Vorbis_5_0  = { FrontLeft, FrontCenter, FrontRight, RearLeft, RearRight };
+		const Layout		 Vorbis_5_1  = { FrontLeft, FrontCenter, FrontRight, RearLeft, RearRight, LFE };
+		const Layout		 Vorbis_6_1  = { FrontLeft, FrontCenter, FrontRight, SideLeft, SideRight, RearCenter, LFE };
+		const Layout		 Vorbis_7_1  = { FrontLeft, FrontCenter, FrontRight, SideLeft, SideRight, RearLeft, RearRight, LFE };
+
+		const Layout		 ALSA_5_0    = { FrontLeft, FrontRight, RearLeft, RearRight, FrontCenter };
+		const Layout		 ALSA_5_1    = { FrontLeft, FrontRight, RearLeft, RearRight, FrontCenter, LFE };
 	};
 
 	class BOCA_DLL_EXPORT Format
