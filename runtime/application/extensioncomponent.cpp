1ee0b5e7d9f734a20d6b76ed75f0f88c153cc62b
Robert Kausch <robert.kausch@freac.org>
2015-11-06 17:21:56 +0000

    Updated source file headers to clearly state the license version.

--- runtime/application/extensioncomponent.cpp
+++ runtime/application/extensioncomponent.cpp
@@ -1,8 +1,10 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2008 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the "GNU General Public License".
+  * modify it under the terms of the GNU General Public License as
+  * published by the Free Software Foundation, either version 2 of
+  * the License, or (at your option) any later version.
   *
   * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
   * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
