05da04cca649a6f21fdd1cacbfa6dd8e2db8cbdc
Robert Kausch <robert.kausch@freac.org>
2016-11-16 21:15:40 +0000

    Fix for compiling on newer OS X versions.

--- components/output/coreaudio/coreaudio.cpp
+++ components/output/coreaudio/coreaudio.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2016 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -16,6 +16,7 @@
 namespace CA
 {
 #	include <CoreServices/CoreServices.h>
+#	include <CoreAudio/AudioHardware.h>
 #	include <AudioUnit/AudioUnit.h>
 };
 
