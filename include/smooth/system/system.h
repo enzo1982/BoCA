2fb019386b8eae96c9afb589a5a23507b8370ad6
Robert Kausch <robert.kausch@freac.org>
2016-11-27 22:22:59 +0000

    Updated smooth header files.

--- include/smooth/system/system.h
+++ include/smooth/system/system.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2015 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2016 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -64,6 +64,7 @@ namespace smooth
 				static String		 GetPersonalFilesDirectory(PersonalFilesType = PersonalFilesGeneric);
 				static String		 GetProgramFilesDirectory();
 				static String		 GetApplicationDataDirectory();
+				static String		 GetResourcesDirectory();
 				static String		 GetTempDirectory();
 		};
 	};
