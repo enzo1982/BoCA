f415b025e8ab52749398e39a1bcd19e391595262
Robert Kausch <robert.kausch@freac.org>
2017-03-07 14:40:55 +0000

    Added missing method EncoderComponentExternal::SetOutputFormat.

--- include/boca/application/external/encodercomponent.h
+++ include/boca/application/external/encodercomponent.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -39,7 +39,9 @@ namespace BoCA
 
 				virtual Bool		 SetAudioTrackInfo(const Track &nTrack)	{ track = nTrack; format = nTrack.GetFormat(); return True; }
 
+				virtual Bool		 SetOutputFormat(Int);
 				virtual String		 GetOutputFileExtension() const;
+
 				virtual Int		 GetNumberOfPasses() const;
 
 				virtual Bool		 IsThreadSafe() const;
--- runtime/application/external/encodercomponent.cpp
+++ runtime/application/external/encodercomponent.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -31,6 +31,11 @@ BoCA::AS::EncoderComponentExternal::EncoderComponentExternal(ComponentSpecs *spe
 BoCA::AS::EncoderComponentExternal::~EncoderComponentExternal()
 {
 	if (configLayer != NIL) Object::DeleteObject(configLayer);
+}
+
+Bool BoCA::AS::EncoderComponentExternal::SetOutputFormat(Int n)
+{
+	return True;
 }
 
 String BoCA::AS::EncoderComponentExternal::GetOutputFileExtension() const
