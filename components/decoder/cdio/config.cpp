4020c5bf23cc8ed55875a4aa86c5c95d275e587a
Robert Kausch <robert.kausch@freac.org>
2017-04-13 22:02:33 +0000

    Disable drive configuration if no drives are available.

--- components/decoder/cdio/config.cpp
+++ components/decoder/cdio/config.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -38,6 +38,8 @@ BoCA::ConfigureCDIO::ConfigureCDIO()
 
 	if (info != NIL)
 	{
+		if (info->GetNumberOfDevices() == 0) group_drive->Deactivate();
+
 		for (Int i = 0; i < info->GetNumberOfDevices(); i++)
 		{
 			const Device	&device = info->GetNthDeviceInfo(i);
