b979f94efe63b61ea6c36d0318729110f717d7e2
Robert Kausch <robert.kausch@freac.org>
2017-07-26 18:37:04 +0000

    Updated smooth header files.

--- include/smooth/gui/window/window.h
+++ include/smooth/gui/window/window.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2015 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2017 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -43,7 +43,7 @@ namespace smooth
 		const Short	 WF_NOTASKBUTTON	= 8;
 		const Short	 WF_THINBORDER		= 16;
 
-		const Short	 WF_NOTITLE		= 32768;
+		const Short	 WF_NOTITLE		= 16384;
 
 		const Short	 WO_SEPARATOR		= 1;
 		const Short	 WO_NOSEPARATOR		= 2;
