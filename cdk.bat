ebc5b72c35aa0937075bb1f0f9a60dbc7dadfa12
Robert Kausch <robert.kausch@freac.org>
2017-09-02 17:28:02 +0200

    Preparing snapshot release.

--- cdk.bat
+++ cdk.bat
@@ -5,7 +5,7 @@ set PATH=%CDK_INSTALL_PATH%\system\bin
 set BUILD_WIN32=True
 set BUILD_X86=True
 
-echo fre:ac Component Development Kit v1.1 Beta 1
+echo fre:ac Component Development Kit v1.1 Alpha 20170902
 echo Copyright (C) 2001-2017 Robert Kausch
 
 bash
--- cdk64.bat
+++ cdk64.bat
@@ -5,7 +5,7 @@ set PATH=%CDK_INSTALL_PATH%\system64\bin
 set BUILD_WIN32=True
 set BUILD_X86_64=True
 
-echo fre:ac Component Development Kit v1.1 Beta 1 (x64)
+echo fre:ac Component Development Kit v1.1 Alpha 20170902 (x64)
 echo Copyright (C) 2001-2017 Robert Kausch
 
 bash
