73bfcc45ed50f79c74514e92f1cc1efff046441d
Robert Kausch <robert.kausch@freac.org>
2017-10-01 14:14:40 +0200

    Updated smooth header files.

--- include/smooth/misc/hash/crc64.h
+++ include/smooth/misc/hash/crc64.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2016 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2017 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -37,6 +37,7 @@ namespace smooth
 							 CRC64();
 							~CRC64();
 			public:
+				static UnsignedInt64	 Compute(const UnsignedByte *, Int);
 				static UnsignedInt64	 Compute(const Buffer<UnsignedByte> &);
 		};
 	};
