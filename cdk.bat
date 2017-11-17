6acceec090771522741f96c4f6be95223e8bbbbf
Robert Kausch <robert.kausch@freac.org>
2017-11-17 22:47:56 +0100

    Create etc folder before starting bash.

--- cdk.bat
+++ cdk.bat
@@ -2,6 +2,8 @@
 set CDK_INSTALL_PATH=%CD%
 set PATH=%CDK_INSTALL_PATH%\system\bin
 
+mkdir %CDK_INSTALL_PATH%\system\etc 2> nul
+
 set BUILD_WIN32=True
 set BUILD_X86=True
 
--- cdk64.bat
+++ cdk64.bat
@@ -2,6 +2,8 @@
 set CDK_INSTALL_PATH=%CD%
 set PATH=%CDK_INSTALL_PATH%\system64\bin
 
+mkdir %CDK_INSTALL_PATH%\system64\etc 2> nul
+
 set BUILD_WIN32=True
 set BUILD_X86_64=True
 
