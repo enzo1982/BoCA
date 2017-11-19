0444c75edd1efb30e940fb81a4ef789c265ca32a
Robert Kausch <robert.kausch@freac.org>
2017-11-19 15:19:35 +0100

    Prepare for new alpha release.

--- cdk.bat
+++ cdk.bat
@@ -7,7 +7,7 @@ mkdir %CDK_INSTALL_PATH%\system\etc 2> nul
 set BUILD_WIN32=True
 set BUILD_X86=True
 
-echo fre:ac Component Development Kit v1.1 Alpha 20170902
+echo fre:ac Component Development Kit v1.1 Alpha 20171119
 echo Copyright (C) 2001-2017 Robert Kausch
 
 bash
