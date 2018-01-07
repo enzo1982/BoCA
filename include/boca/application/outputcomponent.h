001e27df76f759fa6c0b9ba710c88d885708a21f
Robert Kausch <robert.kausch@freac.org>
2018-01-07 14:29:28 +0100

    Add missing qualifier to fix inheritance.

--- include/boca/application/outputcomponent.h
+++ include/boca/application/outputcomponent.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -29,7 +29,7 @@ namespace BoCA
 
 				virtual Bool	 Finish();
 
-				virtual Int	 GetPackageSize();
+				virtual Int	 GetPackageSize() const;
 
 				virtual Int	 CanWrite();
 
--- runtime/application/outputcomponent.cpp
+++ runtime/application/outputcomponent.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -20,7 +20,7 @@ BoCA::AS::OutputComponent::~OutputComponent()
 {
 }
 
-Int BoCA::AS::OutputComponent::GetPackageSize()
+Int BoCA::AS::OutputComponent::GetPackageSize() const
 {
 	return specs->func_GetPackageSize(component);
 }
