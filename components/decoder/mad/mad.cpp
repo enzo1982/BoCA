33429a2bf8c1fcb3bcf47f2e2e2878ebfb5f68e1
Robert Kausch <robert.kausch@freac.org>
2018-01-27 17:14:50 +0100

    Fixed handling of invalid Xing/LAME tags.

--- components/decoder/mad/mad.cpp
+++ components/decoder/mad/mad.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -329,7 +329,7 @@ Bool BoCA::DecoderMAD::ParseVBRHeaders(InStream *in)
 	 */
 	XHEADDATA		 data;
 
-	data.toc = NIL;
+	memset(&data, 0, sizeof(XHEADDATA));
 
 	if (GetXingHeader(&data, buffer))
 	{
--- components/decoder/mpg123/mpg123.cpp
+++ components/decoder/mpg123/mpg123.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -342,7 +342,7 @@ Bool BoCA::DecoderMPG123::ParseVBRHeaders(InStream *in)
 	 */
 	XHEADDATA		 data;
 
-	data.toc = NIL;
+	memset(&data, 0, sizeof(XHEADDATA));
 
 	if (GetXingHeader(&data, buffer))
 	{
