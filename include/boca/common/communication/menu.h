7b72a402637487be9694a3e1a39d0e1688ea7cfe
Robert Kausch <robert.kausch@freac.org>
2018-09-07 20:16:23 +0200

    Fix signals referring to wrong Menu class.

--- include/boca/common/communication/menu.h
+++ include/boca/common/communication/menu.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -38,10 +38,10 @@ namespace BoCA
 			 */
 			static Void			 Free();
 		signals:
-			Signal1<Void, Menu *>		 doMenubarOverlay;	// called at application startup
-			Signal1<Void, Menu *>		 doIconbarOverlay;	// called at application startup
+			Signal1<Void, GUI::Menu *>	 doMenubarOverlay;	// called at application startup
+			Signal1<Void, GUI::Menu *>	 doIconbarOverlay;	// called at application startup
 
-			Signal1<Void, Menu *>		 doContextMenuOverlay;	// called before the context menu is displayed
+			Signal1<Void, GUI::Menu *>	 doContextMenuOverlay;	// called before the context menu is displayed
 	};
 };
 
