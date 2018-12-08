388cf87c12cbdba584b9c8bf9a09647f9f46bb90
Robert Kausch <robert.kausch@freac.org>
2018-12-08 17:28:42 +0100

    Add new callback JobList::doAddFiles.

--- include/boca/common/communication/joblist.h
+++ include/boca/common/communication/joblist.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -25,38 +25,38 @@ namespace BoCA
 		private:
 			/* Singleton class, therefore private constructor/destructor
 			 */
-			static JobList			*instance;
+			static JobList				*instance;
 
-							 JobList();
-							~JobList();
+								 JobList();
+								~JobList();
 
-			Bool				 locked;
+			Bool					 locked;
 		public:
 			/* Returns a new or existing instance of JobList
 			 */
-			static JobList			*Get();
+			static JobList				*Get();
 
 			/* Destroys an existing instance of JobList
 			 */
-			static Void			 Free();
+			static Void				 Free();
 
-			Int				 Lock();
-			Int				 Unlock();
+			Int					 Lock();
+			Int					 Unlock();
 		accessors:
-			Bool				 IsLocked();
+			Bool					 IsLocked();
 		signals:
 			/* Signals fired by application to notify components of
 			 * joblist related actions.
 			 */
-			Signal1<Void, const Track &>	 onApplicationAddTrack;
-			Signal1<Void, const Track &>	 onApplicationModifyTrack;
-			Signal1<Void, const Track &>	 onApplicationRemoveTrack;
-			Signal1<Void, const Track &>	 onApplicationSelectTrack;
+			Signal1<Void, const Track &>		 onApplicationAddTrack;
+			Signal1<Void, const Track &>		 onApplicationModifyTrack;
+			Signal1<Void, const Track &>		 onApplicationRemoveTrack;
+			Signal1<Void, const Track &>		 onApplicationSelectTrack;
 
-			Signal0<Void>			 onApplicationRemoveAllTracks;
+			Signal0<Void>				 onApplicationRemoveAllTracks;
 
-			Signal1<Void, const Track &>	 onApplicationMarkTrack;
-			Signal1<Void, const Track &>	 onApplicationUnmarkTrack;
+			Signal1<Void, const Track &>		 onApplicationMarkTrack;
+			Signal1<Void, const Track &>		 onApplicationUnmarkTrack;
 
 			/* Signals fired by components to notify application of
 			 * joblist related actions.
@@ -66,23 +66,25 @@ namespace BoCA
 			 * application signal. Components must not fire the component
 			 * signal again in that case.
 			 */
-			Signal1<Void, const Track &>	 onComponentAddTrack;
-			Signal1<Void, const Track &>	 onComponentModifyTrack;
-			Signal1<Void, const Track &>	 onComponentRemoveTrack;
-			Signal1<Void, const Track &>	 onComponentSelectTrack;
+			Signal1<Void, const Track &>		 onComponentAddTrack;
+			Signal1<Void, const Track &>		 onComponentModifyTrack;
+			Signal1<Void, const Track &>		 onComponentRemoveTrack;
+			Signal1<Void, const Track &>		 onComponentSelectTrack;
 
-			Signal1<Void, const Track &>	 onComponentMarkTrack;
-			Signal1<Void, const Track &>	 onComponentUnmarkTrack;
+			Signal1<Void, const Track &>		 onComponentMarkTrack;
+			Signal1<Void, const Track &>		 onComponentUnmarkTrack;
 
 			/* Callbacks that can be called by components to request
 			 * the corresponding action to be performed.
 			 */
-			Callback0<Bool>			 doRemoveAllTracks;
+			Callback1<Bool, const Array<String> &>	 doAddFiles;
+
+			Callback0<Bool>				 doRemoveAllTracks;
 
 			/* Callbacks that can be called by components to get
 			 * information about current joblist state.
 			 */
-			Callback0<const Array<Track> *>	 getTrackList;
+			Callback0<const Array<Track> *>		 getTrackList;
 	};
 };
 
