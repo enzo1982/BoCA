4e322964efae53b2941b8d43c8350f00ff2f3874
Robert Kausch <robert.kausch@freac.org>
2019-03-07 20:38:15 +0100

    Deactivate SuperFast mode for Core Audio if input and output sample rates differ.

--- components/encoder/coreaudio/coreaudio.cpp
+++ components/encoder/coreaudio/coreaudio.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2018 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2019 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -292,7 +292,7 @@ Bool BoCA::EncoderCoreAudio::Activate()
 	/* Get number of threads to use.
 	 */
 	Bool	 enableParallel	 = config->GetIntValue("Resources", "EnableParallelConversions", True);
-	Bool	 enableSuperFast = config->GetIntValue("Resources", "EnableSuperFastMode", False);
+	Bool	 enableSuperFast = config->GetIntValue("Resources", "EnableSuperFastMode", False) && format.rate == destinationFormat.mSampleRate;
 	Int	 numberOfThreads = enableParallel && enableSuperFast ? config->GetIntValue("Resources", "NumberOfConversionThreads", 0) : 1;
 
 	if (enableParallel && enableSuperFast && numberOfThreads <= 1) numberOfThreads = CPU().GetNumCores() + (CPU().GetNumLogicalCPUs() - CPU().GetNumCores()) / 2;
