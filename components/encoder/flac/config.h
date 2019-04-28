75118d4c21fa90dfc543d7340cb48b3bc7f499ae
Robert Kausch <robert.kausch@freac.org>
2019-04-28 17:51:43 +0200

    Fix indentation.

--- components/encoder/flac/config.h
+++ components/encoder/flac/config.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2017 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2019 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -101,7 +101,7 @@ namespace BoCA
 			Void			 SetBlockSize();
 			Void			 EditBlockSize();
 		public:
-			static const String	ConfigID;
+			static const String	 ConfigID;
 
 						 ConfigureFLAC();
 						~ConfigureFLAC();
