0b51b3d0d27ff1f1daca6ff944fb8e063599463b
Robert Kausch <robert.kausch@freac.org>
2019-06-27 21:01:37 +0200

    Update Monkey's Audio header files.

--- components/decoder/mac/mac/All.h
+++ components/decoder/mac/mac/All.h
@@ -184,12 +184,12 @@ namespace APE
 Global defines
 *****************************************************************************************/
 #define MAC_FILE_VERSION_NUMBER                         3990
-#define MAC_VERSION_STRING                              _T("4.76")
-#define MAC_NAME                                        _T("Monkey's Audio 4.76")
-#define PLUGIN_NAME                                     "Monkey's Audio Player v4.76"
-#define MJ_PLUGIN_NAME                                  _T("APE Plugin (v4.76)")
-#define CONSOLE_NAME                                    _T("--- Monkey's Audio Console Front End (v 4.76) (c) Matthew T. Ashland ---\n")
-#define PLUGIN_ABOUT                                    _T("Monkey's Audio Player v4.76\nCopyrighted (c) 2000-2019 by Matthew T. Ashland")
+#define MAC_VERSION_STRING                              _T("4.78")
+#define MAC_NAME                                        _T("Monkey's Audio 4.78")
+#define PLUGIN_NAME                                     "Monkey's Audio Player v4.78"
+#define MJ_PLUGIN_NAME                                  _T("APE Plugin (v4.78)")
+#define CONSOLE_NAME                                    _T("--- Monkey's Audio Console Front End (v 4.78) (c) Matthew T. Ashland ---\n")
+#define PLUGIN_ABOUT                                    _T("Monkey's Audio Player v4.78\nCopyrighted (c) 2000-2019 by Matthew T. Ashland")
 #define MAC_DLL_INTERFACE_VERSION_NUMBER                1000
 #define ONE_MILLION										1000000
 #ifdef PLATFORM_WINDOWS
