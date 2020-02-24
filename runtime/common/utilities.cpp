424dc8ec70ae343b3e1ba8e040edc4a6a17d0872
Robert Kausch <robert.kausch@freac.org>
2020-02-24 22:54:47 +0100

    Make sure paths end with a delimiter after Utilities::GetAbsolutePathName.

--- runtime/common/utilities.cpp
+++ runtime/common/utilities.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2019 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2020 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -227,6 +227,8 @@ String BoCA::Utilities::GetAbsolutePathName(const String &path)
 #endif
 
 	if (IsRelativePath(pathName)) pathName = GUI::Application::GetApplicationDirectory().Append(pathName);
+
+	if (!pathName.EndsWith(Directory::GetDirectoryDelimiter())) pathName.Append(Directory::GetDirectoryDelimiter());
 
 	return pathName;
 }
