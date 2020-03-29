aca1237777fb847cc7a6814b379c37f4859200c4
Robert Kausch <robert.kausch@freac.org>
2020-03-29 18:42:02 +0200

    Prepare v1.0 release.

--- cdk.bat
+++ cdk.bat
@@ -7,7 +7,7 @@ mkdir %CDK_INSTALL_PATH%\system\etc 2> nul
 set BUILD_WIN32=True
 set BUILD_X86=True
 
-echo fre:ac Component Development Kit v1.1 RC 2
+echo fre:ac Component Development Kit v1.1
 echo Copyright (C) 2001-2020 Robert Kausch
 
 bash
