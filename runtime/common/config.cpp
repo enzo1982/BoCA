d134945d1921065dbe4becefbca76c317eba6da3
Robert Kausch <robert.kausch@freac.org>
2021-04-02 17:26:26 +0200

    Also copy configDir and cacheDir members when copying a configuration.

--- runtime/common/config.cpp
+++ runtime/common/config.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2019 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2021 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -58,6 +58,9 @@ BoCA::Config::Config(const Config &oConfig)
 
 	persistentIntIDs.EnableLocking();
 	persistentIntValues.EnableLocking();
+
+	configDir = oConfig.configDir;
+	cacheDir  = oConfig.cacheDir;
 }
 
 BoCA::Config::~Config()
