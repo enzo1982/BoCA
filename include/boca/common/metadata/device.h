bb10e0ba780033b05d69a348a0f982bf323671eb
Robert Kausch <robert.kausch@freac.org>
2021-05-29 21:07:47 +0200

    Add methods to get the name and an ID of a device.

--- include/boca/common/metadata/device.h
+++ include/boca/common/metadata/device.h
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2019 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2021 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -42,6 +42,11 @@ namespace BoCA
 			String		 path;
 
 			Bool		 canOpenTray;
+
+			/* Helper functions:
+			 */
+			String		 GetName() const;
+			String		 GetID() const;
 
 			/* Class constructor / destructor:
 			 */
--- runtime/common/metadata/device.cpp
+++ runtime/common/metadata/device.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2019 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2021 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -21,4 +21,16 @@ BoCA::Device::Device()
 
 BoCA::Device::~Device()
 {
+}
+
+String BoCA::Device::GetName() const
+{
+	return String(vendor).Append(" ").Append(model).Append(" ").Append(revision).Trim();
+}
+
+String BoCA::Device::GetID() const
+{
+	String	 name = GetName();
+
+	return Number(Int64(name.ComputeCRC32())).ToHexString(8);
 }
