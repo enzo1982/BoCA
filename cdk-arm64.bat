5f4d05a933b73f2e6bf052f70fd40682760edc1d
Robert Kausch <robert.kausch@freac.org>
2021-11-30 02:53:19 +0100

    Add Windows ARM64 toolchain.

---
+++ cdk-arm64.bat
@@ -0,0 +1,14 @@
+@echo off
+set CDK_INSTALL_PATH=%CD%
+set PATH=%CDK_INSTALL_PATH%\system-arm64\usr\bin
+
+mkdir %CDK_INSTALL_PATH%\system-arm64\tmp 2> nul
+
+set MSYSTEM=MINGW64
+set BUILD_WIN32=True
+set BUILD_ARM64=True
+
+echo fre:ac Component Development Kit v1.1.5 (arm64)
+echo Copyright (C) 2001-2021 Robert Kausch
+
+bash
