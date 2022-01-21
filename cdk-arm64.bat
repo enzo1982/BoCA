247c150307beaf96c35b358130842e5f71814817
Robert Kausch <robert.kausch@freac.org>
2022-01-21 19:13:32 +0100

    Prepare v1.0.6 release.

--- cdk-arm64.bat
+++ cdk-arm64.bat
@@ -8,7 +8,7 @@ set MSYSTEM=MINGW64
 set BUILD_WIN32=True
 set BUILD_ARM64=True
 
-echo fre:ac Component Development Kit v1.1.5 (arm64)
+echo fre:ac Component Development Kit v1.1.6 (arm64)
 echo Copyright (C) 2001-2022 Robert Kausch
 
 bash
