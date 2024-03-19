a00789fb3f6d82531963beeb75ba91245049aed1
Robert Kausch <robert.kausch@freac.org>
2024-03-19 23:44:13 +0100

    Fix possible issue reading MD5 checksums using external codecs on non-Windows platforms.

--- runtime/application/external/posix/decodercomponentfile.cpp
+++ runtime/application/external/posix/decodercomponentfile.cpp
@@ -1,5 +1,5 @@
  /* BoCA - BonkEnc Component Architecture
-  * Copyright (C) 2007-2022 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2007-2024 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -47,7 +47,7 @@ String BoCA::AS::DecoderComponentExternalFile::GetMD5(const String &encFileName)
 													 .Replace("(", "\\(").Replace(")", "\\)").Replace("<", "\\<").Replace(">", "\\>")
 													 .Replace("&", "\\&").Replace(";", "\\;").Replace("$", "\\$").Replace("|", "\\|"));
 
-	FILE	*rPipe = popen(String(command).Append(" ").Append(arguments).Append(specs->external_md5_stderr ? "2>&1" : (specs->debug ? NIL : " 2> /dev/null")), "r");
+	FILE	*rPipe = popen(String(command).Append(" ").Append(arguments).Append(specs->external_md5_stderr ? " 2>&1" : (specs->debug ? NIL : " 2> /dev/null")), "r");
 
 	/* Read output into buffer.
 	 */
